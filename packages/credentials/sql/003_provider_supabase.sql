a30f9eb3766055b96609ab6368004745c39c1a7a
Saidur-droid <buildwithsaidur@gmail.com>
2026-09-17 07:18:17 +0600

    feat: allow Supabase provider connections

---
+++ packages/credentials/sql/003_provider_supabase.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+ALTER TABLE provider_connections
+  DROP CONSTRAINT IF EXISTS provider_connections_provider_check;
+
+ALTER TABLE provider_connections
+  ADD CONSTRAINT provider_connections_provider_check
+  CHECK (provider IN ('vercel', 'supabase'));
+
+COMMIT;
